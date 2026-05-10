cefc25141ce218c3f6f935c81ad4c8cf8095628c
pferragud <pferragud@live.com>
2026-05-10 17:56:48 +0200

    Recepción de pushes 2

--- app/src/main/java/com/pmdm/pmdm08_pruegagithub3/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/pmdm/pmdm08_pruegagithub3/MyFirebaseMessagingService.kt
@@ -1,7 +1,9 @@
 package com.pmdm.pmdm08_pruegagithub3
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
+
 
 }
